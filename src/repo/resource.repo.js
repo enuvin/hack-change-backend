8b21cffa834100c267c10389595e4082d79fc77d
Lorriant <brearey4@gmail.com>
2024-11-30 16:59:47 +0900

    added pattern repository

---
+++ src/repo/resource.repo.js
@@ -0,0 +1,34 @@
+const data_dir = '../../data'
+
+const anketa = require(data_dir + '/anketa.json')
+const anketa_scheme = require(data_dir + '/anketa_scheme.json')
+
+const edu = require(data_dir + '/edu.json')
+const edu_scheme = require(data_dir + '/edu_scheme.json')
+
+const fedresurs = require(data_dir + '/fedresurs.json')
+const fedresurs_scheme = require(data_dir + '/fedresurs_scheme.json')
+
+const okb = require(data_dir + '/okb.json')
+const okb_scheme = require(data_dir + '/okb_scheme.json')
+
+module.exports = {
+    resources_list: [
+        {
+            name: anketa_scheme.title,
+            data: anketa
+        },
+        {
+            name: edu_scheme.title,
+            data: edu
+        },
+        {
+            name: fedresurs_scheme.title,
+            data: fedresurs
+        },
+        {
+            name: okb_scheme.title,
+            data: okb
+        },
+    ]
+}
